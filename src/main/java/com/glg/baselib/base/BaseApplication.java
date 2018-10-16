2b570497265850e63cc8b58adcb347c7c715b435
xiaoguo <xiaoguo@icampus.group>
2018-10-16 17:35:06 +0800

    新增特定接口失败重试机制，保证断网恢复功能正常

--- src/main/java/com/glg/baselib/base/BaseApplication.java
+++ src/main/java/com/glg/baselib/base/BaseApplication.java
@@ -2,13 +2,16 @@ package com.glg.baselib.base;
 
 import android.app.Application;
 import android.content.Context;
+import android.graphics.Color;
 import android.support.multidex.MultiDex;
 
 import com.glg.baselib.BuildConfig;
+import com.glg.baselib.R;
 import com.liulishuo.filedownloader.FileDownloader;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
 import com.tencent.mmkv.MMKV;
+import com.weavey.loading.lib.LoadingLayout;
 
 public class BaseApplication extends Application {
     private static BaseApplication instance;
@@ -27,6 +30,8 @@ public class BaseApplication extends Application {
 
 
         FileDownloader.setup(this);
+        initLoadLayout();
+
     }
 
 
@@ -42,5 +47,22 @@ public class BaseApplication extends Application {
     // 获取Application
     public static Context getMyApplication() {
         return instance;
+    }
+
+
+    private void initLoadLayout(){
+        LoadingLayout.getConfig()
+                .setLoadingPageLayout(R.layout.base_loading)
+                .setErrorText("出错啦~请稍后重试！")
+                .setEmptyText("暂无数据")
+                .setNoNetworkText("无网络连接，请检查您的网络···")
+                .setErrorImage(R.drawable.load_error)
+                .setAllPageBackgroundColor(R.color.transparent)
+                .setAllTipTextColor(R.color.tips)
+                .setAllTipTextSize(14)
+                .setReloadButtonText("点我重试")
+                .setReloadButtonTextSize(14)
+                .setReloadButtonTextColor(R.color.tips)
+                .setReloadButtonWidthAndHeight(150,40);
     }
 }
