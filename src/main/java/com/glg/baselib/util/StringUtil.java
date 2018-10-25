604995116bd88870e3a4053c38e940238676577a
xiaoguo <xiaoguo@icampus.group>
2018-10-25 16:10:02 +0800

    修改密码mock以及功能逻辑完成

--- src/main/java/com/glg/baselib/util/StringUtil.java
+++ src/main/java/com/glg/baselib/util/StringUtil.java
@@ -5,11 +5,26 @@ import android.text.TextUtils;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringUtil {
 
     public static String hidePhoneNum(@NonNull String mobile){
        return mobile.replaceAll("(\\d{3})\\d{4}(\\d{4})", "$1****$2");
+    }
+
+
+    public static boolean isPhone(String phone) {
+        String regex = "^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$";
+        if (phone.length() != 11) {
+            return false;
+        } else {
+            Pattern p = Pattern.compile(regex);
+            Matcher m = p.matcher(phone);
+            boolean isMatch = m.matches();
+            return isMatch;
+        }
     }
 
 
